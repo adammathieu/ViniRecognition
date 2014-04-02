22a9b738be0b1ebbe579d3b412f85e107fc77193
Mathieu Adam <adammathieu@gmail.com>
2014-04-02 21:46:52 +0200

    update test file

--- src/test.py
+++ src/test.py
@@ -1,8 +1,18 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('../Images/domtaillandier_appsavennieresctrl_2009_000002.jpg')
+try:
+	img = cv2.imread('../Images/domtaillandier_appsavennieresctrl_2009_000002.jpg',cv2.CV_LOAD_IMAGE_GRAYSCALE)
+except IOError:
+	print "unable to read files"
 
-cv2.imshow('image',img)
+h,w = img.shape
+print h,w
+
+img_resize = cv2.resize(img,(0,0),fx=0.3, fy=0.3)
+h,w = img_resize.shape
+print h,w
+
+cv2.imshow('image',img_resize)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
